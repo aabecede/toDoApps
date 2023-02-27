dae59ede16f2547b0183417b0d1da4b42f9e97df
aabecede <aabdul.jabbar1301@gmail.com>
2023-02-27 11:14:31 +0700

    [Docker] add doker file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 require('dotenv').config();
-const bodyParser = require('body-parser');
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3030;
+const HOST = process.env.HOST || "localhost";
 const activityRouter = require('../routes/activity-router')
 const toDoRouter = require('../routes/to-do-router')
 
 // buat server nya
-app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server Host "${HOST}" running at port: ${PORT}`));
 //activity
 app.use(activityRouter)
 app.use(toDoRouter)
